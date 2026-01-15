5db8ecf26875dc234fd00c32a66b70ec1e7550b7
chiyyy <hyein031114@gmail.com>
2026-01-15 20:26:02 +0900

    fix: BrowserRouter 중복 사용으로 인한 에러 수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById('root')!;
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="479961622565-m2snvui30sshr9p2h8pa7vu68g81q6el.apps.googleusercontent.com">
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
